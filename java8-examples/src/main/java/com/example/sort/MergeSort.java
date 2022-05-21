d1d7ed697f20a63e236d597ff1d15750bc870dd0
vishwasraiborde19 <103375323+vishwasraiborde19@users.noreply.github.com>
2022-05-21 22:39:17 +0100

    test

--- java8-examples/src/main/java/com/example/sort/MergeSort.java
+++ java8-examples/src/main/java/com/example/sort/MergeSort.java
@@ -3,8 +3,6 @@ package com.example.sort;
 import java.util.Arrays;
 
 public class MergeSort {
-	
-	
 
 	public static void mergeSort(int[] parent, int n) {
 		if (n < 2) {
@@ -20,23 +18,22 @@ public class MergeSort {
 		for (int i = mid; i < n; i++) {
 			r[i - mid] = parent[i];
 		}
-		
-		
+
 		mergeSort(l, mid);
 		mergeSort(r, n - mid);
-		
-		
+
 		merge(parent, l, r, mid, n - mid);
 	}
+
 	public static void merge(int[] parent, int[] lhs, int[] rhs, int left_arr_size, int right_arr_size) {
 
-		System.out.print(" in "+ Arrays.toString(parent));
+		System.out.print(" in " + Arrays.toString(parent));
 		System.out.print(" " + Arrays.toString(lhs));
 		System.out.print(" " + Arrays.toString(rhs));
-		
+
 		System.out.print(" " + (left_arr_size));
-		System.out.println(" " +(right_arr_size));
-		
+		System.out.println(" " + (right_arr_size));
+
 		int i = 0, j = 0, k = 0;
 		while (i < left_arr_size && j < right_arr_size) {
 			if (lhs[i] <= rhs[j]) {
@@ -51,18 +48,16 @@ public class MergeSort {
 		while (j < right_arr_size) {
 			parent[k++] = rhs[j++];
 		}
-		
+
 		System.out.println("out " + Arrays.toString(parent));
 		System.out.println("- ");
-		
-		
+
 	}
 
 	public static void main(String[] args) {
 
 		int[] actual = { 100, 7, 6, 2, 3, 4 };
 		MergeSort.mergeSort(actual, actual.length);
-
 
 	}
 
